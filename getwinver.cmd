44d8b02654f2265dd23fb5da979cf88591d0a031
D1G1T4L3CH0 <D1G1T4L3CH0@users.noreply.github.com>
2017-01-29 21:05:18 -0500

    added windows versions
    Updated to add some recent Windows versions.

--- getwinver.cmd
+++ getwinver.cmd
@@ -1,9 +1,11 @@
 @echo off
 :: Get-Win-Ver
 :: Just a simple script to determine the windows version. You can call this from your own scripts. It sets three variables, win_v, win_v_name, and win_v_build. win-v is the version number #.#. win_v_name is the name of the operating system in a string. win_v_build is the build number. Feel free to modify this any way you like or just copy/paste it to your own script. Your welcome. :)
-:: If you need a more specific answer, it might be possible to determine exact version with the build number. This stript is not setupt to do that.
+:: If you need a more specific answer, it might be possible to determine exact version with the build number. This stript is not setup to do that.
 
 :: Information below from: http://msdn.microsoft.com/en-us/library/windows/desktop/ms724832%28v=vs.85%29.aspx
+:: Windows 10					10.0
+:: Windows 8.1					6.3
 :: Windows 8					6.2
 :: Windows Server 2012			6.2
 :: Windows 7					6.1
@@ -31,6 +33,8 @@ if %win_v% EQU 5.2 set win_v_name=Windows Server 2003 / Server 2003 R2 / XP 64-B
 if %win_v% EQU 6.0 set win_v_name=Windows Vista / Server 2008
 if %win_v% EQU 6.1 set win_v_name=Windows 7 / Server 2008 R2
 if %win_v% EQU 6.2 set win_v_name=Windows 8 / Server 2012
+if %win_v% EQU 6.2 set win_v_name=Windows 8.1 / Server 2012 R2
+if %win_v% EQU 10.0 set win_v_name=Windows 10 / Windows Server 2016
 
 :: If you would like the build number in the same variable as the regular version, just uncomment the following line. Or you could modify the above.
 :: The script cannot set the name if you modify it above. The if statements require a number with one decimal or less.
